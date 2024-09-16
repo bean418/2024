4b719ccf3e5e963a09aa3ad1328c7c1b4f360331
bean0418 <imb2an@gmail.com>
2024-09-16 16:45:14 +0900

    edit_numericPart

--- R/PCRlab./ActNH3_0916.R
+++ R/PCRlab./ActNH3_0916.R
@@ -39,33 +39,21 @@ for(h in 1:9){
   
   # Acetone
   if(ncol(at)==9){
-    numeric_data <- as.numeric(unlist(at[2:nrow(at), -2]));numeric_data
-    df_ac = as.data.frame(matrix(numeric_data,
-                                 byrow = F,
-                                 nrow=nr_at))
+    df_at <- as.data.frame(lapply(at[2:nrow(at), -2], as.numeric))
   } else{
-    numeric_data <- as.numeric(unlist(at[2:nrow(at), ]));numeric_data
-    df_ac = as.data.frame(matrix(numeric_data,
-                                 byrow = F,
-                                 nrow=nr_at))
+    df_at <- as.data.frame(lapply(at[2:nrow(at), ], as.numeric))
   }
   
   # NH3
   if(ncol(nh3)==9){
-    numeric_data <- as.numeric(unlist(nh3[2:nrow(nh3), -2]));numeric_data
-    df_nh3 = as.data.frame(matrix(numeric_data,
-                                  byrow = F,
-                                  nrow=nr_nh3))
+    df_nh3 <- as.data.frame(lapply(nh3[2:nrow(nh3), -2], as.numeric))
   } else{
-    numeric_data <- as.numeric(unlist(nh3[2:nrow(nh3), ]));numeric_data
-    df_nh3 = as.data.frame(matrix(numeric_data,
-                                  byrow = F,
-                                  nrow=nr_nh3))
+    df_nh3 <- as.data.frame(lapply(nh3[2:nrow(nh3), ], as.numeric))
   }
   
   
   # rounding
-  df_ac[, c(1, 3, 5, 7)] <- lapply(df_ac[, c(1, 3, 5, 7)],
+  df_at[, c(1, 3, 5, 7)] <- lapply(df_at[, c(1, 3, 5, 7)],
                                    function(x) (round(x / 100) * 100))
   df_nh3[, c(1, 3, 5, 7)] <- lapply(df_nh3[, c(1, 3, 5, 7)],
                                     function(x) (round(x / 100) * 100))
@@ -76,16 +64,16 @@ for(h in 1:9){
   
   # ploting
   temp = 300
-  for(i in seq(from=1, to=ncol(df_ac), by=2)){
+  for(i in seq(from=1, to=ncol(df_at), by=2)){
     # cat("현재 i는", i, "현재 temp는", temp,'\n')
-    x_ac=unique(df_ac[,i])
+    x_at=unique(df_at[,i])
     x_nh3=unique(df_nh3[,i])
     
-    y_ac=c()
+    y_at=c()
     y_nh3=c()
     cnt = 1
-    for(sec in x_ac){
-      y_ac[cnt] = mean(df_ac[,i+1][df_ac[,i] == sec])
+    for(sec in x_at){
+      y_at[cnt] = mean(df_at[,i+1][df_at[,i] == sec])
       
       cnt = cnt + 1
     }
@@ -95,20 +83,20 @@ for(h in 1:9){
       cnt = cnt + 1
     }
     
-    gg_ac = data.frame(x=x_ac,y=y_ac)
+    gg_at = data.frame(x=x_at,y=y_at)
     gg_nh3 = data.frame(x=x_nh3,y=y_nh3)
     
     ggplot() +
-      geom_line(data = gg_ac, aes(x = x, y = y, color = "Acetone")) +
+      geom_line(data = gg_at, aes(x = x, y = y, color = "Acetone")) +
       geom_line(data = gg_nh3, aes(x = x, y = y, color = "NH3")) +
       labs(title = 'Acetone vs NH3',
            x = 'TimeElapsed (sec)',
            y = 'Resistance (Ohm)',
            color = 'Substance') +
       scale_color_manual(values = c("Acetone" = "blue", "NH3" = "red"))
-
     
-    filename <- paste0("./plts/M", h, "_chip_",
+    
+    filename <- paste0("./plts2/M", h, "_chip_",
                        temp, "oC.png")
     ggsave(filename = filename)
     
