969f33b65468995c3be599a95a5f02dbc4764fb2
bean0418 <imb2an@gmail.com>
2024-11-05 11:45:11 +0900

    prepro_R

--- R/GP/241105.R
+++ R/GP/241105.R
@@ -17,9 +17,9 @@ head(df_co2)
 
 # str(df_co2) -> 데이터가 문자열이다.
 # numeric으로 바꿈, 첫 번째 열(시간) 제외, 첫 번째 행(SENSOR) 제외
-df_co2 <- as.data.frame(lapply(df_co2[120:137, 2:81],
+df_co2 <- as.data.frame(lapply(df_co2[2:137, 2:81],
                                as.numeric))
-df_gas <- as.data.frame(lapply(df_gas[120:137, 2:81],
+df_gas <- as.data.frame(lapply(df_gas[2:137, 2:81],
                                as.numeric))
 
 # 열 이름 재설정
@@ -38,12 +38,10 @@ for(i in 1:5){
   }
 }
 
-#plot
-plot(1:18, df_co2$BD3_SEN5)
-plot(1:18, df_gas$BD3_SEN5)
-
 df_co2 <- data.frame(co2=df_co2$BD3_SEN5)
 df_gas <- data.frame(gas=df_gas$BD3_SEN5)
+
+df_co2_org <- df_co2
 
 df_list <- list(df_co2)
 
@@ -72,15 +70,13 @@ rownames(df_gas) <- NULL
 
 # 세번째 클래스
 ##
-df_list <- list(df_co2)
-
-
+df_list <- list(df_co2_org)
 
 for (i in 1:39) {
-  noisy_data <- data.frame(co2 = add_noise( (df_co2$co2+5000) ))
+  noisy_data <- data.frame(co2 = add_noise( (df_co2_org$co2+5000) ))
   df_list[[i+1]] <- noisy_data
 }
-df_list[[1]] <- data.frame(co2 = add_noise(df_co2$co2+5000))
+df_list[[1]] <- data.frame(co2 = add_noise(df_co2_org$co2+5000))
 
 df_co2_5000 <- do.call(rbind, df_list)
 
